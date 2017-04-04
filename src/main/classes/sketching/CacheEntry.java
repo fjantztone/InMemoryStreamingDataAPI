ba119f3b15b7ddac11b1290cd7158de01ba3ae90
fjantztone <heka1203@student.miun.se>
2017-04-05 01:19:13 +0200

    secooond

---
+++ src/main/classes/sketching/CacheEntry.java
@@ -0,0 +1,26 @@
+package sketching;
+
+import java.util.Map;
+
+/**
+ * Created by heka1203 on 2017-04-05.
+ */
+public class CacheEntry implements Map.Entry {
+    String name;
+    int frequency;
+
+    @Override
+    public Object getKey() {
+        return null;
+    }
+
+    @Override
+    public Object getValue() {
+        return null;
+    }
+
+    @Override
+    public Object setValue(Object value) {
+        return null;
+    }
+}
